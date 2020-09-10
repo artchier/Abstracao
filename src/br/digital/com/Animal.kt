0e9fc9a90c2e59d6b853a78cb21a9e73c09e6ddb
artchier <ar-chaves@hotmail.com>
2020-09-09 22:05:58 -0300

    Exemplo Kotlin Abstração

---
+++ src/br/digital/com/Animal.kt
@@ -0,0 +1,8 @@
+package br.digital.com
+
+abstract class Animal(var nome: String) {
+
+    abstract fun comer()
+
+    abstract fun andar()
+}
---
+++ src/br/digital/com/Main.kt
@@ -0,0 +1,12 @@
+package br.digital.com
+
+fun main() {
+
+    val cachorro = Cachorro("cachorro")
+    val pato = Pato("pato")
+    val patolino = Patolino("patolino")
+
+    cachorro.andar()
+    pato.andar()
+    patolino.patolinoAndando()
+}
